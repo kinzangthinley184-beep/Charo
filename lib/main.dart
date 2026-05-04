28decfeef505d01a8fa010474ae458a254a3c450
Kinzang <kinzangthinley184@gmail.com>
2026-05-04 22:54:04 +0700

    Firebase live - app running with Firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'core/app_theme.dart';
 import 'screens/splash_screen.dart';
 import 'state/app_state.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const CharoApp());
 }
 
